b1f314b94b967e23b405a5a7ca1c887f203e9fc8
bobvad <Djfjfj2006@outlook.com>
2025-11-22 15:14:13 +0500

    метод Autorization. Фефелова

--- Ftp/Server/Program.cs
+++ Ftp/Server/Program.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Server
 {
-    internal class Program
+    public class Program
     {
+        public static List<User> Users = new List<User>();
+        public static IPAddress IpAdress;
+        public static int Port;
+
+        public static bool Autorization(string login, string password)
+        {
+            User user = null;
+            user = Users.Find(x => x.login == login && x.password == password);
+            return user != null;
+
+        }
         static void Main(string[] args)
         {
         }
